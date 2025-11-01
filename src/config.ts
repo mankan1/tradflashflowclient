fc56a67a989fbe601610ca2c769a9502e47d7fd6
Manoj Kandlikar <mano.kandlikar@gmail.com>
2025-11-01 09:31:40 -0700

    go

--- src/config.ts
+++ src/config.ts
@@ -9,16 +9,15 @@ export const WS_URL   = `wss://tradeflashflow-production.up.railway.app/`;
 //export const WS_URL   = `ws://${HOST}:${PORT}/`;
 
 export const WATCHLIST = {
-  alpaca: { equities: ["SPY", "QQQ", "DIA", "IWM", "VOO", "IVV"] },
+  alpaca: { equities: ["SPY", "$SPX", "$NDX", "$DJX"] },
   tradier: {
     options: [
-      { underlying: "SPY", expiration: "2025-12-19", strike: 700, right: "C" },
-      { underlying: "$SPX", expiration: "2025-12-19", strike: 6900, right: "C" }
-      { underlying: "$NDX", expiration: "2025-12-19", strike: 26100, right: "C" }
-      { underlying: "SPY", expiration: "2025-12-19", strike: 650, right: "P" },
-      { underlying: "$SPX", expiration: "2025-12-19", strike: 6820, right: "P" }
-      { underlying: "$NDX", expiration: "2025-12-19", strike: 25500, right: "P" }
+      { underlying: "AAPL", expiration: "2025-12-19", strike: 200, right: "C" },
+      { underlying: "NVDA", expiration: "2025-12-19", strike: 150, right: "P" }
+
+
+
+
     ]
   }
 };
-
